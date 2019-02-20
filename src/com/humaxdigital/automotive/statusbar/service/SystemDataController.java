760e15fc5dbf0a8dc0018768596377725d3593ac
kskim2 <kskim2@humaxdigital.com>
2019-02-20 19:57:50 +0900

    ALM #2009952 - fixed data controller

--- src/com/humaxdigital/automotive/statusbar/service/SystemDataController.java
+++ src/com/humaxdigital/automotive/statusbar/service/SystemDataController.java
@@ -12,6 +12,10 @@ import android.telephony.PhoneStateListener;
 import android.telephony.ServiceState;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
+import android.net.TrafficStats;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.util.Log;
 
@@ -20,6 +24,17 @@ public class SystemDataController extends BaseController<Integer> {
     private enum DataStatus { NONE, DATA_4G, DATA_4G_NO, DATA_E, DATA_E_NO }
     private ConnectivityManager mConnectivity = null;
     private TelephonyManager mTelephony = null;
+    private Timer mTimer = new Timer();
+    private TimerTask mNetworkCheckTask = null;
+    private final int LOOP_TIME = 500;
+    private long mTotalRx = 0; 
+    private long mTotalTx = 0;
+    private boolean mUsing = false; 
+    private boolean mIsNetworkAvaliable = false; 
+    private boolean mIsNetworkConnected = false; 
+    private boolean mIsAvaliableActivityType = false; 
+    private int mDataType = 0;
+    private DataStatus mDataStatus = DataStatus.NONE;
 
     public SystemDataController(Context context, DataStore store) {
         super(context, store);
@@ -38,66 +53,105 @@ public class SystemDataController extends BaseController<Integer> {
 
         if ( mTelephony != null ) 
             mTelephony.listen(mPhoneStateListener, PhoneStateListener.LISTEN_SERVICE_STATE);
+        
+        mTotalRx = TrafficStats.getMobileRxBytes();
+        mTotalTx = TrafficStats.getMobileTxBytes();
+        mNetworkCheckTask = new TimerTask() {
+            @Override
+            public void run() {
+                long rx = TrafficStats.getMobileRxBytes();
+                long tx = TrafficStats.getMobileTxBytes();
+                //Log.d(TAG, "r_old="+mTotalRx+", rx="+rx+" ("+(rx-mTotalRx)+"), t_old="+mTotalTx+", tx="+tx+" ("+(tx-mTotalTx)+")"); 
+                if ( (mTotalRx - rx) != 0 || (mTotalTx - tx) != 0 ) {
+                    if ( !mUsing ) {
+                        mUsing = true; 
+                        Log.d(TAG, "r_old="+mTotalRx+", rx="+rx+" ("+(rx-mTotalRx)+"), t_old="+mTotalTx+", tx="+tx+" ("+(tx-mTotalTx)+")"); 
+                        DataStatus status = convertToStatus(mIsNetworkAvaliable 
+                            && mIsNetworkConnected, mDataType, mUsing); // && mIsAvaliableActivityType, mDataType, mUsing); 
+                        if ( mDataStatus != status ) {
+                            mDataStatus = status; 
+                            for ( Listener<Integer> listener : mListeners ) 
+                                listener.onEvent(mDataStatus.ordinal());
+                        }
+                    } 
+                } else {
+                    if ( mUsing ) {
+                        mUsing = false; 
+                        Log.d(TAG, "r_old="+mTotalRx+", rx="+rx+" ("+(rx-mTotalRx)+"), t_old="+mTotalTx+", tx="+tx+" ("+(tx-mTotalTx)+")"); 
+                        DataStatus status = convertToStatus(mIsNetworkAvaliable 
+                            && mIsNetworkConnected, mDataType, mUsing);// && mIsAvaliableActivityType, mDataType, mUsing); 
+                        if ( mDataStatus != status ) {
+                            mDataStatus = status; 
+                            for ( Listener<Integer> listener : mListeners ) 
+                                listener.onEvent(mDataStatus.ordinal());
+                        }
+                    }
+                }
+                mTotalRx = rx; 
+                mTotalTx = tx; 
+            }
+        };
+
+        mTimer.schedule(mNetworkCheckTask, 0, LOOP_TIME);
     }
 
     @Override
     public void disconnect() {
         Log.d(TAG, "disconnect");
+        if ( mNetworkCheckTask != null ) {
+            mNetworkCheckTask.cancel();
+            mTimer.purge();
+            mNetworkCheckTask =  null;
+        }
+
         if ( mTelephony != null ) 
             mTelephony.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
     }
 
     @Override
     public void fetch() {
-        if ( mConnectivity == null || mTelephony == null || mDataStore == null ) return;
-
-        Log.d(TAG, "fetch");
+        if ( mConnectivity == null || mTelephony == null ) return;
 
         final NetworkInfo netInfo = mConnectivity.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
         if (netInfo == null)  return;
 
-        final boolean isAvailable = netInfo.isAvailable();
-        final boolean isConnected = netInfo.isConnectedOrConnecting();
-        int type = mTelephony.getNetworkType();
-        // todo : Check status when wifi is connected
-        if ( isAvailable && isConnected ) {
-            boolean using = usingData(mTelephony.getDataActivity()); 
-            mDataStore.setNetworkData(type, using); 
-        }
-        else {
-            mDataStore.setNetworkData(type, false); 
-        }
+        mIsNetworkAvaliable = netInfo.isAvailable();
+        mIsNetworkConnected = netInfo.isConnectedOrConnecting();
+        mIsAvaliableActivityType = isAvaliableData(mTelephony.getDataActivity()); 
+        mDataType = mTelephony.getNetworkType();
+        mDataStatus = convertToStatus(mIsNetworkAvaliable 
+            && mIsNetworkConnected, mDataType, mUsing); // && mIsAvaliableActivityType, mDataType, mUsing); 
+
+        Log.d(TAG, "fetch:avaliable="+mIsNetworkAvaliable+", connected="+mIsNetworkConnected
+            +", avaliable data="+mIsAvaliableActivityType+", type="+mDataType+", status="+mDataStatus);
     }
 
     @Override
     public Integer get() {
-        if ( mDataStore == null ) return 0; 
-        int type = mDataStore.getNetworkDataType(); 
-        boolean using = mDataStore.getNetworkDataUsing(); 
-        Log.d(TAG, "get:type="+type+", using="+using);
-        return convertToStatus(type, using).ordinal(); 
+        return mDataStatus.ordinal(); 
     }
 
-    private boolean usingData(int direction) {
-        boolean using = false;
+    private boolean isAvaliableData(int direction) {
+        boolean avaliable = false;
         switch(direction) {
             case TelephonyManager.DATA_ACTIVITY_IN:
             case TelephonyManager.DATA_ACTIVITY_OUT: 
             case TelephonyManager.DATA_ACTIVITY_INOUT: 
                 {
-                    using = true;
+                    avaliable = true;
                     break;
                 }
             case TelephonyManager.DATA_ACTIVITY_DORMANT:
             case TelephonyManager.DATA_ACTIVITY_NONE: 
             default: break;
         }
-        Log.d(TAG, "usingData="+using);
-        return using; 
+        Log.d(TAG, "isAvaliableData="+avaliable+", direction="+direction);
+        return avaliable; 
     }
 
-    private DataStatus convertToStatus(int type, boolean using) {
+    private DataStatus convertToStatus(boolean on, int type, boolean using) {
         DataStatus status = DataStatus.NONE; 
+        if ( !on ) return status;
         switch (type) {
             case TelephonyManager.NETWORK_TYPE_GPRS:
             case TelephonyManager.NETWORK_TYPE_EDGE:
@@ -132,28 +186,23 @@ public class SystemDataController extends BaseController<Integer> {
     private final BroadcastReceiver mConnectivityListener = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if ( mConnectivity == null || mTelephony == null || mDataStore == null ) return;
+            if ( mConnectivity == null || mTelephony == null ) return;
             final NetworkInfo netInfo = mConnectivity.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
             if (netInfo == null)  return;
 
-            final boolean isAvailable = netInfo.isAvailable();
-            final boolean isConnected = netInfo.isConnectedOrConnecting();
-            int type = mTelephony.getNetworkType();
-            Log.d(TAG, "onReceive=CONNECTIVITY_ACTION, available="+isAvailable
-                +", connected="+isConnected+", type"+type);
+            mIsNetworkAvaliable = netInfo.isAvailable();
+            mIsNetworkConnected = netInfo.isConnectedOrConnecting();
+            mIsAvaliableActivityType = isAvaliableData(mTelephony.getDataActivity()); 
+            mDataType = mTelephony.getNetworkType();
+            Log.d(TAG, "onReceive=CONNECTIVITY_ACTION, available="+mIsNetworkAvaliable
+                +", connected="+mIsNetworkConnected+", type"+mDataType);
             // todo : Check status when wifi is connected
-            if ( isAvailable && isConnected ) {
-                boolean using = usingData(mTelephony.getDataActivity()); 
-                if ( mDataStore.shouldPropagateNetworkDataUpdate(type, using) ) {
-                    for ( Listener<Integer> listener : mListeners ) 
-                        listener.onEvent(convertToStatus(type, using).ordinal());
-                }
-            }
-            else {
-                if ( mDataStore.shouldPropagateNetworkDataUpdate(type, false) ) {
-                    for ( Listener<Integer> listener : mListeners ) 
-                        listener.onEvent(convertToStatus(type, false).ordinal());
-                }
+            DataStatus status = convertToStatus(mIsNetworkAvaliable 
+                && mIsNetworkConnected, mDataType, mUsing); // && mIsAvaliableActivityType, mDataType, mUsing); 
+            if ( status != mDataStatus ) {
+                mDataStatus = status;
+                for ( Listener<Integer> listener : mListeners ) 
+                    listener.onEvent(mDataStatus.ordinal());
             }
         }
     };
@@ -162,13 +211,14 @@ public class SystemDataController extends BaseController<Integer> {
         @Override
         public void onDataActivity(int direction) {
             if ( mTelephony == null ) return;
-            int type = mTelephony.getNetworkType();
-            boolean using = usingData(mTelephony.getDataActivity()); 
-            boolean shouldPropagate = mDataStore.shouldPropagateNetworkDataUpdate(type, using);
-            Log.d(TAG, "onDataActivity:LISTEN_SERVICE_STATE:type="+type+", using="+using);
-            if ( shouldPropagate ) {
+            mDataType = mTelephony.getNetworkType();
+            mIsAvaliableActivityType = isAvaliableData(mTelephony.getDataActivity()); 
+            DataStatus status = convertToStatus(mIsNetworkAvaliable 
+                && mIsNetworkConnected, mDataType, mUsing); // && mIsAvaliableActivityType, mDataType, mUsing); 
+            if ( status != mDataStatus ) {
+                mDataStatus = status;
                 for ( Listener<Integer> listener : mListeners ) 
-                    listener.onEvent(convertToStatus(type, using).ordinal());
+                    listener.onEvent(mDataStatus.ordinal());
             }
         }
     };
