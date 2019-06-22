c3520acb6af9de0740b5fb6441339491e9d36e13
Byeonghak Kwon <bhkwon@humaxdigital.com>
2019-06-23 01:16:21 +0900

    [System/Policy] Apply animation multiplier to half (0.5) from default (1.0)

--- src/com/humaxdigital/automotive/systemui/SystemUIPolicy.java
+++ src/com/humaxdigital/automotive/systemui/SystemUIPolicy.java
@@ -5,7 +5,9 @@ package com.humaxdigital.automotive.systemui;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.os.ServiceManager;
 import android.provider.Settings;
+import android.view.IWindowManager;
 import android.util.Log;
 
 public class SystemUIPolicy {
@@ -14,8 +16,13 @@ public class SystemUIPolicy {
     private static final String DEFAULT_IMMERSIVE_MODE =
             "immersive.preconfirms=*";
 
+    private static final float DEFAULT_WINDOW_ANIMATION_SCALE = 0.5f;
+    private static final float DEFAULT_TRANSITION_ANIMATION_SCALE = 0.5f;
+    private static final float DEFAULT_ANIMATOR_DURATION_SCALE = 0.5f;
+
     public static void applyPolicies(Context context) {
         applyImmersiveModePolicy(context);
+        applyDefaultAnimationScales(context);
     }
 
     private static void applyImmersiveModePolicy(Context context) {
@@ -25,5 +32,21 @@ public class SystemUIPolicy {
         // See. frameworks/base/services/core/java/com/android/server/policy/PolicyControl.java
         Settings.Global.putString(ContentResolver,
                 Settings.Global.POLICY_CONTROL, DEFAULT_IMMERSIVE_MODE);
+    }
+
+    private static void applyDefaultAnimationScales(Context context) {
+        final IWindowManager wm = IWindowManager.Stub.asInterface(
+                ServiceManager.getService(Context.WINDOW_SERVICE));
+        try {
+            wm.setAnimationScales(
+                new float[] {
+                    DEFAULT_WINDOW_ANIMATION_SCALE,
+                    DEFAULT_TRANSITION_ANIMATION_SCALE,
+                    DEFAULT_ANIMATOR_DURATION_SCALE
+                }
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
