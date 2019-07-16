9e4ddf25ca76fc4e2da4000fee8aa843a5b9115c
kskim2 <kskim2@humaxdigital.com>
2019-07-16 14:14:45 +0900

    [NotificationUI] added support remote views

--- src/com/humaxdigital/automotive/systemui/notificationui/NotificationUI.java
+++ src/com/humaxdigital/automotive/systemui/notificationui/NotificationUI.java
@@ -1,23 +1,30 @@
 package com.humaxdigital.automotive.systemui.notificationui;
 
 import android.content.Context;
+import android.content.res.Resources.NotFoundException; 
 import android.graphics.drawable.Icon;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.RemoteViews;
+import android.widget.FrameLayout;
 import android.view.View;
-
+import android.util.Log;
 import java.util.HashMap;
+
 
 import com.humaxdigital.automotive.systemui.R;
 
 public class NotificationUI extends LinearLayout {
+    private final String TAG = "NotificationUI";
     private enum Flag {
         TITLE,
         TEXT,
         ICON,
-        DOUBLE_LINE
+        DOUBLE_LINE,
+        VIEWS
     }
 
     private Context mContext;
@@ -25,11 +32,13 @@ public class NotificationUI extends LinearLayout {
     private TextView mBody;
     private ImageView mIcon;
     private ImageView mLine;
+    private FrameLayout mViews;
     private HashMap<Flag, Boolean> mFlags = new HashMap<>();
 
     private String mDataTitle = ""; 
     private String mDataText = ""; 
     private Icon mDataIcon = null;
+    private RemoteViews mDataRemoteViews = null; 
 
     public NotificationUI(Context context) {
         super(context);
@@ -42,12 +51,16 @@ public class NotificationUI extends LinearLayout {
         mFlags.put(Flag.TEXT, false);
         mFlags.put(Flag.ICON, false);
         mFlags.put(Flag.DOUBLE_LINE, false);
+        mFlags.put(Flag.VIEWS, false); 
     }
 
     public void inflate() {
         if ( mContext == null ) return;
         LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        if ( mFlags.get(Flag.TITLE) ) {
+        if ( mFlags.get(Flag.VIEWS)) {
+            inflater.inflate(R.layout.notification_remote_view, this, true);
+        }
+        else if ( mFlags.get(Flag.TITLE) ) {
             if ( mFlags.get(Flag.DOUBLE_LINE) ) 
                 inflater.inflate(R.layout.notification_left_double_line, this, true);
             else 
@@ -58,40 +71,54 @@ public class NotificationUI extends LinearLayout {
             else 
                 inflater.inflate(R.layout.notification, this, true);
         }
-        
-        mTitle = (TextView)this.findViewById(R.id.title);
-        mBody = (TextView)this.findViewById(R.id.body);
-        mIcon = (ImageView)this.findViewById(R.id.icon);
-        mLine = (ImageView)this.findViewById(R.id.line);
-
-        if ( mTitle == null || mBody == null || mIcon == null || mLine == null ) return;
        
-        if ( mFlags.get(Flag.TITLE) ) {
-            mTitle.setVisibility(View.VISIBLE); 
-            mLine.setVisibility(View.VISIBLE); 
-            mTitle.setText(mDataTitle);
+        if ( mFlags.get(Flag.VIEWS) ) {
+            mViews = (FrameLayout)this.findViewById(R.id.view_container); 
+            if ( mViews == null ) return; 
+            if ( mDataRemoteViews != null ) {
+                try {
+                    ViewGroup inf_view = (ViewGroup)mDataRemoteViews.apply(mContext, mViews); 
+                    mViews.addView(inf_view, 0); 
+                } catch(NotFoundException e) {
+                    Log.e(TAG, "not found resource !!"); 
+                }
+            }
         } else {
-            mTitle.setVisibility(View.GONE);
-            mLine.setVisibility(View.GONE);
-        }
-        
-        if ( mFlags.get(Flag.TEXT) ) {
-            mBody.setVisibility(View.VISIBLE); 
-            mBody.setText(mDataText);
-        } else {
-            mBody.setVisibility(View.GONE);
-        }
+            mTitle = (TextView)this.findViewById(R.id.title);
+            mBody = (TextView)this.findViewById(R.id.body);
+            mIcon = (ImageView)this.findViewById(R.id.icon);
+            mLine = (ImageView)this.findViewById(R.id.line);
+            
+            if ( mTitle == null || mBody == null || mIcon == null || mLine == null ) return; 
 
-        if ( mFlags.get(Flag.ICON) ) {
-            mIcon.setVisibility(View.VISIBLE); 
-            mIcon.setImageIcon(mDataIcon);
-        } else {
-            mIcon.setVisibility(View.GONE); 
+            if ( mFlags.get(Flag.TITLE) ) {
+                mTitle.setVisibility(View.VISIBLE); 
+                mLine.setVisibility(View.VISIBLE); 
+                mTitle.setText(mDataTitle);
+            } else {
+                mTitle.setVisibility(View.GONE);
+                mLine.setVisibility(View.GONE);
+            }
+            
+            if ( mFlags.get(Flag.TEXT) ) {
+                mBody.setVisibility(View.VISIBLE); 
+                mBody.setText(mDataText);
+            } else {
+                mBody.setVisibility(View.GONE);
+            }
+    
+            if ( mFlags.get(Flag.ICON) ) {
+                mIcon.setVisibility(View.VISIBLE); 
+                mIcon.setImageIcon(mDataIcon);
+            } else {
+                mIcon.setVisibility(View.GONE); 
+            }
         }
     }
 
     public NotificationUI setTitle(String title) {
         if ( title == null || title.equals("") ) return this;
+        Log.d(TAG, "setTitle="+title); 
         mDataTitle = title; 
         mFlags.put(Flag.TITLE, true);
         return this;
@@ -99,6 +126,7 @@ public class NotificationUI extends LinearLayout {
 
     public NotificationUI setBody(String body) {
         if ( body == null || body.equals("") ) return this;
+        Log.d(TAG, "setBody="+body); 
         mDataText = body; 
         mFlags.put(Flag.TEXT, true);
         if ( mDataText.contains("\n") )
@@ -108,8 +136,17 @@ public class NotificationUI extends LinearLayout {
 
     public NotificationUI setIcon(Icon icon) {
         if ( icon == null ) return this;
+        Log.d(TAG, "setIcon="+icon); 
         mDataIcon = icon; 
         mFlags.put(Flag.ICON, true);
+        return this;
+    }
+
+    public NotificationUI setRemoteViews(RemoteViews views) {
+        if ( views == null ) return this;
+        mDataRemoteViews = views.clone();
+        Log.d(TAG, "setRemoteViews="+mDataRemoteViews);  
+        mFlags.put(Flag.VIEWS, true); 
         return this;
     }
 }
