eeece9b79f61146691fd414baf1e75a7be535620
kskim2 <kskim2@humaxdigital.com>
2019-08-13 18:25:25 +0900

    [VolumeUI] fixed quite mode issue when change user

--- src/com/humaxdigital/automotive/systemui/volumedialog/ScenarioQuiteMode.java
+++ src/com/humaxdigital/automotive/systemui/volumedialog/ScenarioQuiteMode.java
@@ -29,35 +29,31 @@ public class ScenarioQuiteMode {
     private Context mContext = null;
     private ContentResolver mContentResolver = null;
     private ContentObserver mModeObserver = null;
+    private ContentObserver mUserObserver = null; 
     private final int VOLUME_MAX = 45;
     private final int QUITE_MODE_MAX = 20;
     private final int QUITE_MODE_VOLUME = 7;
 
-    private HashMap<VolumeUtil.Type,Integer> mQuiteModeLastVolume = new HashMap<>();
-    private HashMap<VolumeUtil.Type,Boolean> mQuiteModeAudioChange = new HashMap<>();
-    private ArrayList<VolumeUtil.Type> mQuiteModeAudioTypeList = new ArrayList<>(); 
-
-    
     private CarAudioManagerEx mCarAudioManagerEx = null;
     private boolean mIsQuiteModeApplying = false; 
     private boolean mNeedToShowUI = false; 
-/*
-    public class VolumeRecovery {
-        public class VolumeDB {
-            public class VolumeValue {
-                public int mLastVolume; 
-                public boolean mIsVolumeChanged; 
-            }
-            private HashMap<VolumeUtil.Type,VolumeValue> mVolumeDB = new HashMap<>();
-        }
-        private int mUser; 
-        private HashMap<int,VolumeRecovery> mUserVolume = new HashMap<>();
-        VolumeRecovery(int userid) {
-            mUser = userid; 
-        }
+
+    public class VolumeValue {
+        public int mLastVolume = 0; 
+        public boolean mIsVolumeChanged = false; 
     }
-*/
-    
+
+    public class VolumeDB {
+        public VolumeValue mVolumeValue = new VolumeValue();
+    }
+
+    public class UserVolume {
+        public HashMap<VolumeUtil.Type, VolumeDB> mVolumeDB = new HashMap<>(); 
+    }
+
+    private HashMap<Integer, UserVolume> mUserVolume = new HashMap<>(); 
+    private ArrayList<VolumeUtil.Type> mAudioTypeList = new ArrayList<>(); 
+
 
     public ScenarioQuiteMode(Context context) {
         if ( context == null ) return;
@@ -65,10 +61,38 @@ public class ScenarioQuiteMode {
         mContentResolver = mContext.getContentResolver();
     }
 
-    private ContentObserver createObserver() {
+    private int getCurrentUser() {
+        int user = Settings.Global.getInt(mContext.getContentResolver(), 
+            CarExtraSettings.Global.USERPROFILE_LAST_DRIVER, 
+            CarExtraSettings.Global.USERPROFILE_LAST_DRIVER_DEFAULT);
+        Log.d(TAG, "getCurrentUser="+user);
+        return user; 
+    }
+
+    private boolean isUserSwiching() {
+        int isUserSwitching = Settings.Global.getInt(mContext.getContentResolver(), 
+            CarExtraSettings.Global.USERPROFILE_USER_SWITCHING_START_FINISH, 
+            CarExtraSettings.Global.FALSE);
+        Log.d(TAG, "isUserSwiching="+isUserSwitching);
+        return (isUserSwitching == CarExtraSettings.Global.FALSE) ? false:true; 
+    }
+
+    private ContentObserver createModeObserver() {
         ContentObserver observer = new ContentObserver(new Handler()) {
             @Override
             public void onChange(boolean selfChange, Uri uri, int userId) {
+                Log.d(TAG, "mode changed"); 
+                quitetModeChanged();
+            }
+        };
+        return observer; 
+    }
+
+    private ContentObserver createUserObserver() {
+        ContentObserver observer = new ContentObserver(new Handler()) {
+            @Override
+            public void onChange(boolean selfChange, Uri uri, int userId) {
+                Log.d(TAG, "user changed"); 
                 quitetModeChanged();
             }
         };
@@ -76,29 +100,38 @@ public class ScenarioQuiteMode {
     }
     
     public ScenarioQuiteMode init() {
-        mQuiteModeAudioTypeList.add(VolumeUtil.Type.RADIO_AM);
-        mQuiteModeAudioTypeList.add(VolumeUtil.Type.RADIO_FM);
-        mQuiteModeAudioTypeList.add(VolumeUtil.Type.USB);
-        mQuiteModeAudioTypeList.add(VolumeUtil.Type.ONLINE_MUSIC);
-        mQuiteModeAudioTypeList.add(VolumeUtil.Type.CARLIFE_MEDIA); 
-        mQuiteModeAudioTypeList.add(VolumeUtil.Type.BT_AUDIO);
-        mModeObserver = createObserver(); 
-        if ( mContentResolver != null ) mContentResolver.registerContentObserver(
-            Settings.System.getUriFor(CarExtraSettings.System.SOUND_QUIET_MODE_ON), 
-            false, mModeObserver, UserHandle.USER_CURRENT); 
+        mAudioTypeList.add(VolumeUtil.Type.RADIO_AM);
+        mAudioTypeList.add(VolumeUtil.Type.RADIO_FM);
+        mAudioTypeList.add(VolumeUtil.Type.USB);
+        mAudioTypeList.add(VolumeUtil.Type.ONLINE_MUSIC);
+        mAudioTypeList.add(VolumeUtil.Type.CARLIFE_MEDIA); 
+        mAudioTypeList.add(VolumeUtil.Type.BT_AUDIO);
+        mModeObserver = createModeObserver(); 
+        mUserObserver = createUserObserver();
+        if ( mContentResolver != null ) {
+            mContentResolver.registerContentObserver(
+                Settings.System.getUriFor(CarExtraSettings.System.SOUND_QUIET_MODE_ON), 
+                false, mModeObserver, UserHandle.USER_CURRENT); 
+            mContentResolver.registerContentObserver(
+                Settings.Global.getUriFor(CarExtraSettings.Global.USERPROFILE_LAST_DRIVER), 
+                false, mUserObserver, UserHandle.USER_CURRENT); 
+        }
+        
         if ( isQuiteMode() ) applyQuiteMode();
         return this; 
     }
 
     public void deinit() {
-        mQuiteModeAudioTypeList.clear();
-        mQuiteModeLastVolume.clear();
-        mQuiteModeAudioChange.clear();
-        if ( mContentResolver != null ) 
+        mAudioTypeList.clear();
+        if ( mContentResolver != null ) {
             mContentResolver.unregisterContentObserver(mModeObserver); 
+            mContentResolver.unregisterContentObserver(mUserObserver); 
+        }
+            
         mContentResolver = null;
         mCarAudioManagerEx = null;
         mModeObserver = null;
+        mUserObserver = null;
         mContext = null;
     }
     
@@ -106,14 +139,76 @@ public class ScenarioQuiteMode {
         mCarAudioManagerEx = mgr; 
     }
 
+    private int getQuiteModeLastVolume(VolumeUtil.Type type) {
+        int volume = 0; 
+        int user = getCurrentUser(); 
+        UserVolume uv = mUserVolume.get(user);
+        if ( uv == null ) return volume; 
+        VolumeDB vd = uv.mVolumeDB.get(type);
+        if ( vd == null ) return volume; 
+        volume = vd.mVolumeValue.mLastVolume; 
+        Log.d(TAG, "getQuiteModeLastVolume : type="+type+", volume="+volume); 
+        return volume; 
+    }
+
+    private boolean isQuiteModeLastVolumeChanged(VolumeUtil.Type type) {
+        boolean changed = false; 
+        int user = getCurrentUser(); 
+        UserVolume uv = mUserVolume.get(user);
+        if ( uv == null ) return changed; 
+        VolumeDB vd = uv.mVolumeDB.get(type);
+        if ( vd == null ) return changed; 
+        changed = vd.mVolumeValue.mIsVolumeChanged; 
+        Log.d(TAG, "isQuiteModeLastVolumeChanged : type="+type+", changed="+changed); 
+        return changed;  
+    }
+
+    private void updateQuiteModeLastVolume(VolumeUtil.Type type, int volume, boolean changed) {
+        int user = getCurrentUser(); 
+        UserVolume uv = mUserVolume.get(user);
+        if ( uv == null ) {
+            uv = new UserVolume(); 
+            mUserVolume.put(user, uv); 
+        }
+        VolumeDB vd = uv.mVolumeDB.get(type);
+        if ( vd == null ) {
+            vd = new VolumeDB();
+            uv.mVolumeDB.put(type, vd); 
+        } 
+        vd.mVolumeValue.mLastVolume = volume; 
+        vd.mVolumeValue.mIsVolumeChanged = changed;
+        Log.d(TAG, "updateQuiteModeLastVolume : type="+type+", volume="+volume); 
+    }
+
+    private void updateQuiteModeLastVolume(VolumeUtil.Type type, int volume) {
+        updateQuiteModeLastVolume(type, volume, false); 
+    }
+    
+    private void updateQuiteModeLastVolumeChanged(VolumeUtil.Type type, boolean changed) {
+        int user = getCurrentUser(); 
+        UserVolume uv = mUserVolume.get(user);
+        if ( uv == null ) {
+            uv = new UserVolume(); 
+            mUserVolume.put(user, uv); 
+        }
+        VolumeDB vd = uv.mVolumeDB.get(type);
+        if ( vd == null ) {
+            vd = new VolumeDB();
+            uv.mVolumeDB.put(type, vd); 
+        } 
+        vd.mVolumeValue.mIsVolumeChanged = changed; 
+        Log.d(TAG, "updateQuiteModeLastVolumeChanged : type="+type+", changed="+changed);
+    }
+
     private void quitetModeChanged() {
+        Log.d(TAG, "quitetModeChanged"); 
+        if ( isUserSwiching() ) return; 
         if ( isQuiteMode() ) applyQuiteMode();
         else {
-            if ( mQuiteModeLastVolume.isEmpty() || mQuiteModeAudioChange.isEmpty() ) return;
-            for ( VolumeUtil.Type type : mQuiteModeAudioTypeList ) {
-                boolean changed = mQuiteModeAudioChange.get(type); 
+            for ( VolumeUtil.Type type : mAudioTypeList ) {
+                boolean changed = isQuiteModeLastVolumeChanged(type); 
                 if ( !changed ) {
-                    int volume = mQuiteModeLastVolume.get(type);
+                    int volume = getQuiteModeLastVolume(type);
                     setAudioVolume(VolumeUtil.convertToMode(type), volume);
                     Log.d(TAG, "onChange:type="+type+", volume="+volume);
                 }
@@ -148,7 +243,7 @@ public class ScenarioQuiteMode {
 
     public int getQuiteModeMax(int mode) {
         int max = VOLUME_MAX; 
-        for ( VolumeUtil.Type type : mQuiteModeAudioTypeList ) {
+        for ( VolumeUtil.Type type : mAudioTypeList ) {
             if ( mode != VolumeUtil.convertToMode(type) ) continue; 
             max = QUITE_MODE_MAX; 
         }
@@ -158,15 +253,14 @@ public class ScenarioQuiteMode {
     private void applyQuiteMode() {
         if ( mCarAudioManagerEx == null ) return;
         try {
-            for ( VolumeUtil.Type type : mQuiteModeAudioTypeList ) {
+            for ( VolumeUtil.Type type : mAudioTypeList ) {
                 int mode = VolumeUtil.convertToMode(type);
                 int volume = mCarAudioManagerEx.getVolumeForLas(mode);
-                mQuiteModeLastVolume.put(type, volume);
-                mQuiteModeAudioChange.put(type, false);
+                updateQuiteModeLastVolume(type, volume); 
                 Log.d(TAG, "applyQuiteMode:type="+type+", mode="+mode+", volume="+volume);
             }
             mIsQuiteModeApplying = true;
-            for ( VolumeUtil.Type type : mQuiteModeAudioTypeList ) {
+            for ( VolumeUtil.Type type : mAudioTypeList ) {
                 int mode = VolumeUtil.convertToMode(type);
                 int volume = mCarAudioManagerEx.getVolumeForLas(mode);
                 
@@ -180,24 +274,26 @@ public class ScenarioQuiteMode {
         }
     }
 
+    
     public boolean checkQuietMode(int mode, int volume) {
         if ( !isQuiteMode() ) return false; 
 
         mNeedToShowUI = false;
 
-        for ( VolumeUtil.Type type : mQuiteModeAudioTypeList ) {
+        for ( VolumeUtil.Type type : mAudioTypeList ) {
             if ( mode != VolumeUtil.convertToMode(type) ) continue; 
 
             if ( !mIsQuiteModeApplying && volume != QUITE_MODE_VOLUME ) {
-                mQuiteModeAudioChange.put(type, true);
+
+                updateQuiteModeLastVolumeChanged(type, true);
                 if ( type == VolumeUtil.Type.RADIO_AM || type == VolumeUtil.Type.RADIO_FM 
                 || type == VolumeUtil.Type.USB || type == VolumeUtil.Type.ONLINE_MUSIC 
                 || type == VolumeUtil.Type.CARLIFE_MEDIA ) {
-                    mQuiteModeAudioChange.put(VolumeUtil.Type.RADIO_AM, true);
-                    mQuiteModeAudioChange.put(VolumeUtil.Type.RADIO_FM, true);
-                    mQuiteModeAudioChange.put(VolumeUtil.Type.USB, true);
-                    mQuiteModeAudioChange.put(VolumeUtil.Type.ONLINE_MUSIC, true);
-                    mQuiteModeAudioChange.put(VolumeUtil.Type.CARLIFE_MEDIA, true);
+                    updateQuiteModeLastVolumeChanged(VolumeUtil.Type.RADIO_AM, true);
+                    updateQuiteModeLastVolumeChanged(VolumeUtil.Type.RADIO_FM, true);
+                    updateQuiteModeLastVolumeChanged(VolumeUtil.Type.USB, true);
+                    updateQuiteModeLastVolumeChanged(VolumeUtil.Type.ONLINE_MUSIC, true);
+                    updateQuiteModeLastVolumeChanged(VolumeUtil.Type.CARLIFE_MEDIA, true);
                 }
             }
             
@@ -223,7 +319,7 @@ public class ScenarioQuiteMode {
             mContentResolver.unregisterContentObserver(mModeObserver); 
         }
         Log.d(TAG, "userRefresh");
-        mModeObserver = createObserver(); 
+        mModeObserver = createModeObserver(); 
         mContentResolver.registerContentObserver(
             Settings.System.getUriFor(CarExtraSettings.System.SOUND_QUIET_MODE_ON), 
             false, mModeObserver, UserHandle.USER_CURRENT); 
